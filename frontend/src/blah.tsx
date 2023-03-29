54f687a0e0cc1b051d3ed61f47b255f08465e020
allisonwinder <103597663+allisonwinder@users.noreply.github.com>
2023-03-28 21:33:51 -0600

    so many things

---
+++ frontend/src/blah.tsx
@@ -0,0 +1,11 @@
+function TopBanner (){
+    return (
+        <div>
+          <img src="./logo192.png" alt="companyLogo"></img>
+        <   h1>Allison's React Site</h1>  
+        </div>
+        
+    );
+}
+
+export default TopBanner
